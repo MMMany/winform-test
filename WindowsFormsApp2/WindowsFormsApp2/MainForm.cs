d7a8dc9997c4a68f68feda43d3e25401a161b1f8
Youngman Jo <youngman7.jo@gmail.com>
2024-01-04 05:17:47 +0900

    initial commit

---
+++ WindowsFormsApp2/WindowsFormsApp2/MainForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var filter = @"Test file (*.txt)|*.txt";
+            openFileDialog.Filter = filter;
+            saveFileDialog.Filter = filter;
+            
+            logTextBox.ReadOnly = true;
+            logTextBox.BackColor = System.Drawing.SystemColors.Window;
+        }
+
+        private void Debug(string message)
+        {
+            var action = new Action(() =>
+            {
+                if (logTextBox.TextLength == 0)
+                {
+                    logTextBox.AppendText(message);
+                }
+                else
+                {
+                    logTextBox.AppendText($"\n{message}");
+                }
+                logTextBox.ScrollToCaret();
+            });
+            
+            if (logTextBox.InvokeRequired)
+            {
+                logTextBox.BeginInvoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private WindowInfo CurrentWindow = null;
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            var res = MessageBox.Show(
+                text: "Will be close?",
+                caption: "Program close",
+                buttons: MessageBoxButtons.YesNo,
+                icon: MessageBoxIcon.Question,
+                defaultButton: MessageBoxDefaultButton.Button2
+            );
+            if (res == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                // disposing
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void openMenuItem_Click(object sender, EventArgs e)
+        {
+            var res = openFileDialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                var target = openFileDialog.FileName;
+                Debug($"Opening file : {target}");
+                var sr = new StreamReader(target);
+                var data = sr.ReadToEnd();
+                Debug($"Open complete (length: {data.Length})");
+            }
+            
+        }
+
+        private void saveMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void saveAsMenuItem_Click(object sender, EventArgs e)
+        {
+            var res = saveFileDialog.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                var target = saveFileDialog.FileName;
+                Debug($"Saving to '{target}'");
+                //var sw = new StreamWriter(target);
+            }
+        }
+
+        private void settingsMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void closeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            if (findTextBox.TextLength != 0)
+            {
+                var window = FindWindow(findTextBox.Text);
+                if (window != null)
+                {
+                    findResultLabel.Text = $"{window.Name} ({window.Bounds})";
+                    Debug($"Window found : {window.Name} / {window.Bounds}");
+                    CurrentWindow = window;
+                }
+                else
+                {
+                    findResultLabel.Text = "Not Found";
+                    Debug($"Window not found : {findTextBox.Text}");
+                    CurrentWindow = null;
+                }
+            }
+        }
+
+        private void moveWindowButton_Click(object sender, EventArgs e)
+        {
+            if (posXTestBox.TextLength == 0 || posYTextBox.TextLength == 0)
+                return;
+
+            if (CurrentWindow != null)
+            {
+                var x = int.Parse(posXTestBox.Text);
+                var y = int.Parse(posYTextBox.Text);
+                var res = CurrentWindow.Move(x, y);
+                findResultLabel.Text = $"{CurrentWindow.Name} ({CurrentWindow.Bounds})";
+            }
+        }
+
+        private void resizeWindowButton_Click(object sender, EventArgs e)
+        {
+            if (widthTextBox.TextLength == 0 || heightTextBox.TextLength == 0)
+                return;
+
+            if (CurrentWindow != null)
+            {
+                var width = int.Parse(widthTextBox.Text);
+                var height = int.Parse(heightTextBox.Text);
+                var res = CurrentWindow.Resize(width, height);
+                findResultLabel.Text = $"{CurrentWindow.Name} ({CurrentWindow.Bounds})";
+            }
+        }
+
+        private void numericTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void findTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.findButton_Click(null, null);
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/WindowsUtils.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public int X { get => Left; set => Left = value; }
+        public int Y { get => Top; set => Top = value; }
+        public int Width { get => Right - Left; }
+        public int Height { get => Bottom - Top; }
+        public System.Drawing.Point Location { get => new System.Drawing.Point(Left, Top); }
+        public System.Drawing.Size Size { get => new System.Drawing.Size(Width, Height); }
+
+        public override bool Equals(object obj)
+        {
+            return obj is RECT rECT &&
+                   Left == rECT.Left &&
+                   Top == rECT.Top &&
+                   Right == rECT.Right &&
+                   Bottom == rECT.Bottom &&
+                   Width == rECT.Width &&
+                   Height == rECT.Height &&
+                   EqualityComparer<Point>.Default.Equals(Location, rECT.Location) &&
+                   EqualityComparer<Size>.Default.Equals(Size, rECT.Size);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1082896862;
+            hashCode = hashCode * -1521134295 + Left.GetHashCode();
+            hashCode = hashCode * -1521134295 + Top.GetHashCode();
+            hashCode = hashCode * -1521134295 + Right.GetHashCode();
+            hashCode = hashCode * -1521134295 + Bottom.GetHashCode();
+            hashCode = hashCode * -1521134295 + Width.GetHashCode();
+            hashCode = hashCode * -1521134295 + Height.GetHashCode();
+            hashCode = hashCode * -1521134295 + Location.GetHashCode();
+            hashCode = hashCode * -1521134295 + Size.GetHashCode();
+            return hashCode;
+        }
+
+        public System.Drawing.Rectangle ToRectangle()
+        {
+            return new System.Drawing.Rectangle(Location, Size);
+        }
+
+        public override string ToString()
+        {
+            return $"RECT[{Left}, {Top}, {Width}, {Height}]";
+        }
+
+        public static RECT operator +(RECT rt1, RECT rt2)
+        {
+            return new RECT(
+                rt1.Left + rt2.Left,
+                rt1.Top + rt2.Top,
+                rt1.Right + rt2.Right,
+                rt1.Bottom + rt2.Bottom);
+        }
+        public static RECT operator -(RECT rt1, RECT rt2)
+        {
+            return new RECT(
+                rt1.Left - rt2.Left,
+                rt1.Top - rt2.Top,
+                rt1.Right - rt2.Right,
+                rt1.Bottom - rt2.Bottom);
+        }
+        public static bool operator ==(RECT rt1, RECT rt2)
+        {
+            return (
+                rt1.Left == rt2.Left
+                && rt1.Top == rt2.Top
+                && rt1.Right == rt2.Right
+                && rt1.Bottom == rt2.Bottom);
+        }
+        public static bool operator !=(RECT rt1, RECT rt2)
+        {
+            return (
+                rt1.Left != rt2.Left
+                || rt1.Top != rt2.Top
+                || rt1.Right != rt2.Right
+                || rt1.Bottom != rt2.Bottom);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is POINT pOINT &&
+                   X == pOINT.X &&
+                   Y == pOINT.Y;
+        }
+        public override int GetHashCode()
+        {
+            int hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"POINT[{X}, {Y}]";
+        }
+
+        public static POINT operator +(POINT pt1, POINT pt2)
+        {
+            return new POINT(pt1.X + pt2.X, pt1.Y + pt2.Y);
+        }
+        public static POINT operator -(POINT pt1, POINT pt2)
+        {
+            return new POINT(pt1.X - pt2.X, pt1.Y - pt2.Y);
+        }
+        public static bool operator ==(POINT pt1, POINT pt2)
+        {
+            return (pt1.X == pt2.X && pt1.Y == pt2.Y);
+        }
+        public static bool operator !=(POINT pt1, POINT pt2)
+        {
+            return (pt1.X != pt2.X || pt1.Y != pt2.Y);
+        }
+    }
+
+    public struct Test
+    {
+        public int x1;
+        public int x2;
+    }
+
+    public class WindowInfo
+    {
+        public IntPtr Handle { get; private set; }
+        private RECT _bounds;
+        public RECT Bounds { get => _bounds; }
+        public string Name { get; private set; }
+
+        public WindowInfo(IntPtr handle, RECT bounds, string name)
+        {
+            Handle = handle;
+            _bounds = bounds;
+            Name = name;
+        }
+
+        public bool IsValid()
+        {
+            return IsWindow(Handle);
+        }
+
+        public int Move(Point location)
+        {
+            return _MoveWindow(location.X, location.Y, Bounds.Width, Bounds.Height);
+        }
+
+        public int Move(int x, int y)
+        {
+            return _MoveWindow(x, y, Bounds.Width, Bounds.Height);
+        }
+
+        public int Resize(Size size)
+        {
+            return _MoveWindow(Bounds.X, Bounds.Y, size.Width, size.Height);
+        }
+
+        public int Resize(int width, int height)
+        {
+            return _MoveWindow(Bounds.X, Bounds.Y, width, height);
+        }
+
+        private int _MoveWindow(int x, int y, int width, int height)
+        {
+            var success = MoveWindow(Handle, x, y, width, height, true);
+            var errCode = 1;
+            if (success)
+            {
+                _bounds.X = x;
+                _bounds.Y = y;
+                _bounds.Right = x + width;
+                _bounds.Bottom = y + height;
+            }
+            else
+            {
+                errCode = Marshal.GetLastWin32Error();
+            }
+            return errCode;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool MoveWindow(IntPtr hWnd, int x, int y, int width, int height, bool bRepaint);
+    }
+
+    public partial class MainForm : Form
+    {
+        private WindowInfo FindWindow(string windowName)
+        {
+            if (windowName == null || windowName.Trim().Length == 0) 
+                return null;
+
+            WindowInfo ret = null;
+
+            var handle = FindWindow(null, windowName);
+            if (handle != IntPtr.Zero)
+            {
+                var length = GetWindowTextLength(handle);
+                var builder = new StringBuilder(length);
+                GetWindowText(handle, builder, length + 1);
+                GetWindowRect(handle, out RECT bounds);
+                ret = new WindowInfo(handle, bounds, builder.ToString());
+            }
+            else
+            {
+                EnumWindows(new EnumWindowsProc((hWnd, param) =>
+                {
+                    var length = GetWindowTextLength(hWnd);
+                    if (length == 0)
+                        return true;
+
+                    var builder = new StringBuilder(length);
+                    GetWindowText(hWnd, builder, length + 1);
+                    if (builder.ToString().StartsWith(windowName))
+                    {
+                        GetWindowRect(hWnd, out RECT bounds);
+                        ret = new WindowInfo(hWnd, bounds, builder.ToString());
+                        return false;
+                    }
+
+                    return true;
+                }), IntPtr.Zero);
+            }
+
+            return ret;
+        }
+    }
+
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.I4)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+    }
+}
